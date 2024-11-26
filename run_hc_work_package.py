2374dbcb6bddec9310d43cbcc3b7cfe66d85f71f
Roberto Marquez <roberto.marquez@zepben.com>
2024-11-26 13:43:24 +1100

    Updated the hosting capacity runner to work with the latest changes to the eas client and hosting capacity service.
    Signed-off-by: Roberto Marquez <roberto.marquez@zepben.com>

--- run_hc_work_package.py
+++ run_hc_work_package.py
@@ -1,36 +1,33 @@
 import asyncio
+import sys
 from datetime import datetime
 
-from zepben.eas.client.work_package import WorkPackageConfig, ModelConfig, TimePeriod
+from zepben.eas.client.work_package import WorkPackageConfig, TimePeriod
 
-from utils import get_client, get_config
+from utils import get_client, get_config, print_result, get_config_dir
 
 
-async def main():
-    config = get_config()
-    eas_client = get_client()
+async def main(argv):
+    config_dir = get_config_dir(argv)
+    config = get_config(config_dir)
+    eas_client = get_client(config_dir)
     result = await eas_client.async_run_hosting_capacity_work_package(
         WorkPackageConfig(
             config["feeders"],
             config["forecast_years"],
             config["scenarios"],
-            model_config=ModelConfig(
-                load_time=TimePeriod(
-                    start_time=datetime.fromisoformat(config["load_time"]["start"]),
-                    end_time=datetime.fromisoformat(config["load_time"]["end"])
-                )
-            )
+            load_time=TimePeriod(
+                start_time=datetime.fromisoformat(config["load_time"]["start"]),
+                end_time=datetime.fromisoformat(config["load_time"]["end"])
+            ),
+            name=config["work_package_name"]
         )
     )
 
-    if "data" in result:
-        print(f'work_package_id={result["data"]["runHostingCapacityWorkPackage"]}')
-    else:
-        print(f"errors:\n{result['errors'].join(', ')}")
-
+    print_result(result)
     await eas_client.aclose()
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(sys.argv))
--- utils.py
+++ utils.py
@@ -4,24 +4,26 @@ from typing import Dict
 from zepben.eas.client.eas_client import EasClient
 
 
-def get_config():
-    config = read_json_config("config.json")
+def get_config_dir(argv):
+    return argv[1] if len(argv) > 1 else "."
+
+
+def get_config(config_dir):
+    config = read_json_config(f"{config_dir}/config.json")
     config["feeders"] = list({f for f in config["feeders"]})
     config["forecast_years"] = list({f for f in config["forecast_years"]})
     config["scenarios"] = list({f for f in config["scenarios"]})
     return config
 
 
-def get_client():
-    auth_config = read_json_config("auth_config.json")
+def get_client(config_dir):
+    auth_config = read_json_config(f"{config_dir}/auth_config.json")
+
     return EasClient(
         host=auth_config["eas_server"]["host"],
         port=auth_config["eas_server"]["port"],
         protocol=auth_config["eas_server"]["protocol"],
-        client_id=auth_config["eas_server"].get("client_id"),
-        username=auth_config["eas_server"].get("username"),
-        password=auth_config["eas_server"].get("password"),
-        client_secret=auth_config["eas_server"].get("client_secret"),
+        access_token=auth_config["eas_server"]["access_token"],
         verify_certificate=auth_config["eas_server"].get("verify_certificate", True),
         ca_filename=auth_config["eas_server"].get("ca_filename")
     )
@@ -32,3 +34,10 @@ def read_json_config(config_file_path: str) -> Dict:
     config_dict = json.load(file)
     file.close()
     return config_dict
+
+
+def print_result(result):
+    if "data" in result:
+        print(f'work_package_id=\n{result["data"]["runWorkPackage"]}')
+    else:
+        print("Errors:\n", "\n".join(err["message"] for err in result['errors']))
