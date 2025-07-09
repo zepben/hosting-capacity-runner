627a959dd1c5ead63415a36bb9915f6cd5798447
Jimmy Tung <jimmy.tung@zepben.com>
2025-07-09 15:41:05 +1000

    Update run work package example to cover different permutation of setup.
    Signed-off-by: Jimmy Tung <jimmy.tung@zepben.com>

--- run_work_package.py
+++ run_work_package.py
@@ -2,9 +2,10 @@ import asyncio
 import sys
 from datetime import datetime
 
+from zepben.eas import FeederConfigs, ForecastConfig, FixedTimeLoadOverride
 from zepben.eas.client.work_package import WorkPackageConfig, TimePeriod, ResultProcessorConfig, StoredResultsConfig, \
     MetricsResultsConfig, WriterConfig, WriterOutputConfig, EnhancedMetricsConfig, GeneratorConfig, ModelConfig, \
-    FeederScenarioAllocationStrategy, SolveConfig, RawResultsConfig
+    FeederScenarioAllocationStrategy, SolveConfig, RawResultsConfig, FeederConfig, TimePeriodLoadOverride, FixedTime
 
 from utils import get_client, get_config, print_run, get_config_dir
 
@@ -13,16 +14,81 @@ async def main(argv):
     config_dir = get_config_dir(argv)
     config = get_config(config_dir)
     eas_client = get_client(config_dir)
+
+    # Forecast Config example set up
+    # This can be set up through config file or by hard coding in the variables.
+    forecast_config = ForecastConfig(
+        feeders=config["feeders"],
+        years=config["forecast_years"],
+        scenarios=config["scenarios"],
+        load_time=TimePeriod(
+            start_time=datetime.fromisoformat(config["load_time"]["start1"]),
+            end_time=datetime.fromisoformat(config["load_time"]["end1"]),
+            load_overrides=
+            {"nmi1": TimePeriodLoadOverride(
+                # Override profile needs to have entries covering a single day or a year.
+                load_watts=[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0,
+                            16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0],
+                # Amount of entry between watts and var must match if both watts and vars exists.
+                load_var=[2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0,
+                          16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0],
+                gen_var=None,
+                gen_watts=None
+            )}
+
+        )
+    )
+
+    # Feeder Configs example set up
+    # More entries can be added into the configs list base on the config supplied (or hard coded)
+    feeder_configs = FeederConfigs(
+        configs=[
+            FeederConfig(
+                feeder=config["feeders"][0],
+                years=config["forecast_years"],
+                scenarios=config["scenarios"],
+                load_time=TimePeriod(
+                    start_time=datetime.fromisoformat(config["load_time"]["start2"]),
+                    end_time=datetime.fromisoformat(config["load_time"]["end2"]),
+                    load_overrides=
+                    {"nmi1": TimePeriodLoadOverride(
+                        # Override profile needs to have entries covering a single day or a year.
+                        load_watts=[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0,
+                                    16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0],
+                        # Amount of entry between watts and var must match if both watts and vars exists.
+                        load_var=[2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0,
+                                  16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0],
+                        gen_var=None,
+                        gen_watts=None
+                    )}
+                )
+            ),
+            # Fixed time example
+            FeederConfig(
+                feeder=config["feeders"][1],
+                years=config["forecast_years"],
+                scenarios=config["scenarios"],
+                load_time=FixedTime(
+                    time=datetime.fromisoformat(config["load_time"]["start1"]),
+                    load_overrides=
+                    {"nmi2": FixedTimeLoadOverride(
+                        # Fixed time load override supports any number of entries.
+                        load_watts=[1.0, 2.0, 3.0],
+                        # Same matching entry logic applies here when both watts and var exists.
+                        load_var=[2.0, 3.0, 4.0],
+                        gen_var=None,
+                        gen_watts=None
+                    )}
+                )
+            )
+        ]
+    )
+
     result = await eas_client.async_run_hosting_capacity_work_package(
         WorkPackageConfig(
             name=config["work_package_name"],
-            feeders=config["feeders"],
-            years=config["forecast_years"],
-            scenarios=config["scenarios"],
-            load_time=TimePeriod(
-                start_time=datetime.fromisoformat(config["load_time"]["start"]),
-                end_time=datetime.fromisoformat(config["load_time"]["end"])
-            ),
+            syf_config=feeder_configs, # Uncomment this and comment out the below syf_config assignment to use feeder configs.
+            # syf_config=forecast_config, # Uncomment this and comment out the above syf_config assignment to use forecast config.
             generator_config=GeneratorConfig(
                 model=ModelConfig(
                     vmax_pu=1.2,
